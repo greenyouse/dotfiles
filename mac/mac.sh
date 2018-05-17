531d1034891ee81b30ce4f25f10424cca36eda59
Ed Babcock <greenyouse@hotmail.com>
2018-05-16 23:46:40 -0500

    refactor: osx, group node with other pkg downloads

--- mac/mac.sh
+++ mac/mac.sh
@@ -25,13 +25,9 @@ get-pkg vscode "https://go.microsoft.com/fwlink/?LinkID=620882"
 pushd $INSTALL_DIR
 wget https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg
 wget https://pqrs.org/osx/karabiner/files/Karabiner-Elements-12.0.0.dmg
+wget https://nodejs.org/dist/v8.11.2/node-v8.11.2.pkg
 wget https://download-installer.cdn.mozilla.net/pub/devedition/releases/61.0b5/linux-x86_64/en-US/firefox-61.0b5.tar.bz2
 tar -xfj firefox-61.0b5.tar.bz2
-popd
-
-# download node + npm, LTS
-pushd $INSTALL_DIR
-wget https://nodejs.org/dist/v8.11.2/node-v8.11.2.pkg
 popd
 
 # move karabiner config
